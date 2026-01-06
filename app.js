e3d61d8e64d6432dc41021c65c3a2b4c0318535d
Nidhi <nidhirajput291205@gmail.com>
2026-01-06 21:51:57 +0530

    Added Update feature

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app=express();
 const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
 const path=require("path");
-
+const methodOverride=require("method-override");
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
 main().then(()=>{
@@ -20,6 +20,7 @@ async function main(){
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 
 app.get("/",(req,res)=>{
@@ -53,7 +54,19 @@ app.post("/listings",async (req,res)=>{
     res.redirect("/listings");
 })
 
+//edit route
+app.get("/listings/:id/edit",async (req,res)=>{
+     let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
 
+//update route
+app.put("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing})
+    res.redirect(`/listings/${id}`);
+});
 
 // app.get("/testListing",async (req,res)=>{
 //     let sampleListing=new Listing({

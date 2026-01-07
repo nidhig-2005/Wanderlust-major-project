9489337b415aa6a601183a9a22481f2ee17eaed7
Nidhi <nidhirajput291205@gmail.com>
2026-01-07 19:49:58 +0530

    Integrated ejs-mate for reusable EJS layouts

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing=require("./models/listing.js");
 const path=require("path");
 const methodOverride=require("method-override");
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+const ejsMate=require("ejs-mate");
 
 main().then(()=>{
     console.log("connected to DB");
@@ -21,6 +22,8 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 
 app.get("/",(req,res)=>{
@@ -83,7 +86,6 @@ app.delete("/listings/:id",async(req,res)=>{
 //         location:"Calangute,Goa",
 //         country:"India",
 //     });
-
 // await sampleListing.save();
 // console.log("sample was saved");
 // res.send("successful testing");

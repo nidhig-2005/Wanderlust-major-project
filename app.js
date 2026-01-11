fca9731579d433e257cbd513b3e062d3814e9edd
Nidhi <nidhirajput291205@gmail.com>
2026-01-11 17:26:41 +0530

    Added custom ExpressError

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express=require("express");
+const express= require("express");
 const app=express();
 const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
@@ -7,6 +7,7 @@ const methodOverride=require("method-override");
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 const ejsMate=require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync.js")
+const ExpressError=require("./utils/ExpressError.js");
 
 main().then(()=>{
     console.log("connected to DB");
@@ -52,32 +53,38 @@ app.get("/listings/:id", async (req,res)=>{
 
 //Create route
 app.post("/listings",wrapAsync(async (req,res,next)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"Send Valid Data for Listing");
+    }
         const newListing=new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
 }));
 
 //edit route
-app.get("/listings/:id/edit",async (req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
      let {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
-});
+}));
 
 //update route
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",wrapAsync(async(req,res)=>{
+     if(!req.body.listing){
+        throw new ExpressError(400,"Send Valid Data for Listing");
+    }
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing})
     res.redirect(`/listings/${id}`);
-});
+}));
 
 
 //delete route 
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndDelete(id,{...req.body.listing})
     res.redirect("/listings");
-});
+}));
 
 // app.get("/testListing",async (req,res)=>{
 //     let sampleListing=new Listing({
@@ -92,9 +99,16 @@ app.delete("/listings/:id",async(req,res)=>{
 // res.send("successful testing");
 // })
 
+
+//for handling all other routes not defined
+app.use((req,res,next)=>{
+    next(new ExpressError(404,"Page Not Found!"))
+});
+
 //middleware
 app.use((err,req,res,next)=>{
-    res.send("something went wrong!")
+    let {statusCode=500,message="Something went wrong"}=err;
+    res.status(statusCode).send(message);
 })
 
 app.listen(8080,()=>{

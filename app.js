ea2dc2dee55ce7bb3e7bba626825ed61d67b71c6
Nidhi <nidhirajput291205@gmail.com>
2026-01-13 16:41:35 +0530

    Created a post route for reviews

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const ejsMate=require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync.js")
 const ExpressError=require("./utils/ExpressError.js");
 const { listingSchema }=require("./schema.js");
+const Review=require("./models/review.js");
 
 main().then(()=>{
     console.log("connected to DB");
@@ -99,6 +100,17 @@ app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     await Listing.findByIdAndDelete(id,{...req.body.listing})
     res.redirect("/listings");
 }));
+
+//reviews
+//post route
+app.post("/listings/:id/reviews",async(req,res)=>{
+let listing=await Listing.findById(req.params.id);
+let newReview=new Review(req.body.review);
+listing.reviews.push(newReview);
+await newReview.save();
+await listing.save();
+res.redirect(`/listings/${listing._id}`);
+});
 
 // app.get("/testListing",async (req,res)=>{
 //     let sampleListing=new Listing({

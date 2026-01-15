1ba91ef4324b4c463b7bd253a0c52d1f655393c9
Nidhi <nidhirajput291205@gmail.com>
2026-01-15 17:22:11 +0530

    Added session

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ejsMate=require("ejs-mate");
 const ExpressError=require("./utils/ExpressError.js");
 const listings=require("./routes/listing.js");
 const reviews=require("./routes/review.js");
+const session=require("express-session");
 
 main().then(()=>{
     console.log("connected to DB");
@@ -27,6 +28,18 @@ app.use(methodOverride("_method"));
 app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"public")));``
 
+const sessionOptions={
+    secret:"mysupersecretcode",
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        expires:Date.now()+7*24*60*60*1000,
+        maxAge:7*24*60*60*1000,
+        httpOnly:true
+},
+};
+
+app.use(session(sessionOptions));
 
 app.get("/",(req,res)=>{
     res.send("root working");

4a75c0eb8b975d2c92a17b1f56a687597ff8353c
Nidhi <nidhirajput291205@gmail.com>
2026-01-17 17:33:51 +0530

    connected login page

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,7 @@ const wrapAsync=require("../utils/wrapAsync.js");
 const ExpressError=require("../utils/ExpressError.js");
 const { listingSchema}=require("../schema.js");
 const Listing=require("../models/listing.js");
+const {isLoggedIn}=require("../middleware.js");
 
 
 //validate listing middleware
@@ -25,7 +26,7 @@ router.get("/",async (req,res)=>{
     });
 
 //new route
-router.get("/new",(req,res)=>{
+router.get("/new",isLoggedIn,(req,res)=>{
     res.render("listings/new.ejs");
 });
 
@@ -42,7 +43,7 @@ router.get("/:id", async (req,res)=>{
 
 
 //Create route
-router.post("/",validateListing,
+router.post("/",isLoggedIn,validateListing,
     wrapAsync(async (req,res,next)=>{
         const newListing=new Listing(req.body.listing);
     await newListing.save();
@@ -51,7 +52,7 @@ router.post("/",validateListing,
 }));
 
 //edit route
-router.get("/:id/edit",wrapAsync(async (req,res)=>{
+router.get("/:id/edit",isLoggedIn,wrapAsync(async (req,res)=>{
      let {id}=req.params;
     const listing=await Listing.findById(id);
      if(!listing){
@@ -62,7 +63,7 @@ router.get("/:id/edit",wrapAsync(async (req,res)=>{
 }));
 
 //update route
-router.put("/:id",validateListing,wrapAsync(async(req,res)=>{
+router.put("/:id",isLoggedIn,validateListing,wrapAsync(async(req,res)=>{
      if(!req.body.listing){
         throw new ExpressError(400,"Send Valid Data for Listing");
     }
@@ -74,7 +75,7 @@ router.put("/:id",validateListing,wrapAsync(async(req,res)=>{
 
 
 //delete route 
-router.delete("/:id",wrapAsync(async(req,res)=>{
+router.delete("/:id",isLoggedIn,wrapAsync(async(req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndDelete(id);
     req.flash("success","Listing deleted");

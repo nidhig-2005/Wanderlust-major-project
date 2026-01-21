8537126de3168650f07c7ebd97690f8d30da66ca
Nidhi <nidhirajput291205@gmail.com>
2026-01-21 21:26:08 +0530

    added map marker and marker popup

--- public/js/map.js
+++ public/js/map.js
@@ -3,6 +3,13 @@
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         style:"mapbox://styles/mapbox/streets-v12", // style URL
-        center: [77.209, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
+
+    const marker=new mapboxgl.Marker({color:'red'})
+    .setLngLat(listing.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({offset:25 })
+        .setHTML(`<h4>${listing.title}</h4>
+            <p>Exact location will be provided after booking</p>`))
+    .addTo(map);
